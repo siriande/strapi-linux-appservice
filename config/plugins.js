b41805d8c879109f9c930a3ce67201a8acd77d77
Leela Siri Ande <lande@microsoft.com>
2025-02-25 02:13:34 +0530

    Strapi linux app service updates - upgrade Strapi version(v5.10.3) - add Azure email provider plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+export default ({ env }) => ({
+    email: {
+        config: {
+          provider: 'strapi-provider-email-azure',
+          providerOptions: {
+            endpoint: env('AZURE_EMAIL_SERVICE_ENDPOINT'),
+          },
+          settings: {
+            defaultFrom: env('FROM_EMAIL'),
+          },
+        },
+      }
+});
