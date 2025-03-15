13e00ddea4a3e7de7308976c95cc28e046772cbe
Siri Ande <69354202+siriande@users.noreply.github.com>
2025-03-15 23:47:47 +0530

    Adding Azure storage account for media uploads

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,25 @@
 export default ({ env }) => ({
-    email: {
-        config: {
-          provider: 'strapi-provider-email-azure',
-          providerOptions: {
-            endpoint: env('AZURE_EMAIL_SERVICE_ENDPOINT'),
-          },
-          settings: {
-            defaultFrom: env('FROM_EMAIL'),
-          },
-        },
+  email: {
+    config: {
+      provider: 'strapi-provider-email-azure',
+      providerOptions: {
+        endpoint: env('AZURE_EMAIL_SERVICE_ENDPOINT')
+      },
+      settings: {
+        defaultFrom: env('FROM_EMAIL')
       }
+    }
+  },
+  upload: {
+    config: {
+        provider: "strapi-provider-upload-azure-storage",
+        providerOptions: {
+            authType: env("STORAGE_AUTH_TYPE", "default"),
+            account: env("STORAGE_ACCOUNT_NAME"),
+            accountKey: env("STORAGE_ACCOUNT_KEY"),
+            containerName: env("BLOB_CONTAINER_NAME"),
+            defaultPath: "uploads"
+        }
+    }
+  }
 });
